7a528c55cb6300a3efac53b3ccf289adddc7508b
Ubuntu <ubuntu@ip-172-31-5-72.ap-northeast-1.compute.internal>
2021-01-03 03:11:37 +0000

    トップページヘッダーの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
   devise_for :users
   get 'pages/index'
   get 'pages/show'
+  resources :rooms
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
