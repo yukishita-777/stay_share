0cf5ace9c93ed7e0589b95467ac71b5f0055ab76
Ubuntu <ubuntu@ip-172-31-5-72.ap-northeast-1.compute.internal>
2021-01-03 02:21:25 +0000

    roomテーブル、コントローラ作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_004927) do
+ActiveRecord::Schema.define(version: 2021_01_03_021731) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "room_name"
+    t.text "room_introduction"
+    t.integer "fee"
+    t.text "address"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
