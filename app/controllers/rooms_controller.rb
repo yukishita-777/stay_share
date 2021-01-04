a7a0e6e71e83595611c42b50441026dd0606b434
Ubuntu <ubuntu@ip-172-31-5-72.ap-northeast-1.compute.internal>
2021-01-04 10:42:44 +0000

    ルーム登録フォーム作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,23 @@
 class RoomsController < ApplicationController
+    
+    def new
+        @room = Room.new
+    end
+    
+    def create
+        @room = Room.new(room_name: room_params[:room_name], room_introduction: room_params[:room_introduction], fee: room_params[:fee], address: room_params[:address], 
+                        image: room_params[:image], user_id: current_user.id)
+                        
+        if @room.save
+            flash[:notice] = ルーム登録が完了しました。
+            redirect_to :root
+        else
+            render "new"
+        end
+    end
+    
+    private 
+    def room_params
+        params.require(:room).permit(:room_name, :room_introduction, :fee, :address, :image)
+    end
 end
