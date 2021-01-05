692b284f003b7f988ee72f6263b2d4a07a5ce6e1
Ubuntu <ubuntu@ip-172-31-5-72.ap-northeast-1.compute.internal>
2021-01-05 12:28:15 +0000

    ユーザー登録修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  root to: 'pages#index'
   devise_for :users
+  root to: 'pages#index'
   get 'pages/index'
   get 'pages/show'
   resources :rooms
-  resources :users
+  resources :users, only:[:show, :edit, :update]
   resources :registrations, only:[:new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
